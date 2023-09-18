22397546ed601c134686272b9a1f61e0cc81cd9c
nifbastiani <108945028+nifbastiani@users.noreply.github.com>
2023-09-18 16:05:37 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,22 @@
+escrever = (msg)=> alert (msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+div = (a,b) => a / b;
+mult = (a,b) => a * b;
+raiz = a => Math.sqrt(a);
+eq2grau = (a,b,c) => {
+    if (a == 0) return "Não é uma equação do Segundo Grau.";
+    let delta = sub(mult(b,b), mult (4, mult (a,c)));
+    if (delta < 0) return "Não possui raiz real.";
+    if (delta == 0) return "x1 = x2 =" + div(-b, mult (2,a));
+    return "x1"+ div (soma(-b, raiz(delta)), mult (2,a)) +
+           "x2"+ div (sub(-b, raiz(delta)), mult (2,a));
+} 
+let a ="";
+let b ="";
+let op ="";
+let resultado ="";
+function digitando(tecla){
+    a =+ tecla;
+    alert(a);
+}
